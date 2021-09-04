7edee37af16ccdcf44afb70af2070a6a5757cc6c
Sergio Cano <canosergio90@gmail.com>
2021-09-04 15:56:51 -0300

    useState

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CounterApp } from './components/01-useState/CounterApp';
+// import { HooksApp } from './HooksApp';
+
+ReactDOM.render(
+  <CounterApp />,
+  document.getElementById('root')
+);
