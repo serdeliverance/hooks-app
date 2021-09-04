2147b998557e86936256c037da95a0da48a78a9a
Sergio Cano <canosergio90@gmail.com>
2021-09-04 18:26:47 -0300

    useEffect - cleaning resource example

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,17 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
 
+    const [coors, setCoors] = useState({ x:0, y:0 })
+
+    const { x, y } = coors
+
     useEffect(() => {
-        console.log('component has been mounted')
+
+        const mouseMove = (e) => {
+            const coors = { x: e.x, y: e.y }
+            setCoors(coors)
+        }
+
+        window.addEventListener('mousemove', mouseMove)
+
         return () => {
-            console.log('component unmounted')
+            window.removeEventListener('mousemove', mouseMove)
         }
     }, [])
 
     return (
         <div>
             <h3>Eres genial!</h3>
+            <p>
+                x: {x} y: {y}
+            </p>
         </div>
     )
 }
