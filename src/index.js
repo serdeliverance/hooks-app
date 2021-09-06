b0b920125c51ea715ed1504e992a2e53fd4c07b1
Sergio Cano <canosergio90@gmail.com>
2021-09-06 20:16:41 -0300

    useRef example

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import { CounterWithCustomHook } from './components/01-useState/CounterWithCusto
 import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 import { SimpleForm } from './components/02-useEffect/SimpleForm';
 import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 // import { HooksApp } from './HooksApp';
 
 ReactDOM.render(
-  <MultipleCustomHooks />,
+  <RealExampleRef />,
   document.getElementById('root')
 );
