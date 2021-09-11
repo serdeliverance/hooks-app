05a8e3c6fa88d36e3d743aab2a214ea22ae5f585
Sergio Cano <canosergio90@gmail.com>
2021-09-10 22:35:05 -0300

    useContext intro

--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
 
 export const MainApp = () => {
+
+    const user = {
+        id: 1234,
+        name: 'Fernando',
+        email: 'fernando.herrera@gmail.com'
+    }
+
     return (
-        <AppRouter />
+        <UserContext.Provider value={user}>
+            <AppRouter />
+        </UserContext.Provider>
     )
 }
